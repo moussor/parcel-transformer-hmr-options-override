096235b9ee51239c5d8b6ada5cfb1ee4323524cc
moussor <themoussor@gmail.com>
2022-03-01 15:41:02 +0100

    add test for options.hmrOptions (if it is null, prevents builds)

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ module.exports = new Transformer({
     // Retrieve the asset's source code and source map.
     let source = await asset.getCode();
     const sourceMap = await asset.getMap();
+    console.log('hmr options?', options, options.hmrOptions);
+    if(options.hmrOptions) {
 
-    const { host: hmrHost, port: hmrPort } = options.hmrOptions;
-    if (hmrHost) {
-      source = source.replace(/HMR_HOST = null/g, `HMR_HOST = "${hmrHost}"`);
-    }
+      const { host: hmrHost, port: hmrPort } = options.hmrOptions;
+      if (hmrHost) {
+        source = source.replace(/HMR_HOST = null/g, `HMR_HOST = "${hmrHost}"`);
+      }
 
-    if (hmrPort) {
-      source = source.replace(/HMR_PORT = null/g, `HMR_PORT = ${hmrPort}`);
+      if (hmrPort) {
+        source = source.replace(/HMR_PORT = null/g, `HMR_PORT = ${hmrPort}`);
+      }
     }
 
     asset.setCode(source);
     asset.setMap(sourceMap);
+
 
     // Return the asset
     return [asset];
